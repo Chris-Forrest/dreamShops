33ee09acb29c092c8d63e52aad26f52951486e7c
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-28 09:21:53 -0500

    Update Product.java
    added code for Product variables initialized and brought in category and List for images

--- dream-shops/src/main/java/com/example/dream_shops/model/Product.java
+++ dream-shops/src/main/java/com/example/dream_shops/model/Product.java
@@ -1,5 +1,18 @@
 package com.example.dream_shops.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Product {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    private Category category;
+
+    private List<Image> images;
 
 }
