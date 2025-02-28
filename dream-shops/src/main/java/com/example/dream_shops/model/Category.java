852cfd38453f8e6ea32506a3877f8ac9c5bfb8e9
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-02-28 09:12:30 -0500

    added package and classes
    added the model package and three classes for category Image and product added initial code for category Long String and created a List for products

---
+++ dream-shops/src/main/java/com/example/dream_shops/model/Category.java
@@ -0,0 +1,10 @@
+package com.example.dream_shops.model;
+import java.util.List;
+
+public class Category {
+    private Long id; 
+    private String name;
+
+    private List<Product> products;
+
+}
