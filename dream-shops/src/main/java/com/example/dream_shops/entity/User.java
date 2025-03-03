73f5f5fb536e598e461600800779e7167e092999
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-03-03 12:22:34 -0500

    Update User.java
    initializing user class with defining variables and relationship to order class

--- dream-shops/src/main/java/com/example/dream_shops/entity/User.java
+++ dream-shops/src/main/java/com/example/dream_shops/entity/User.java
@@ -1,5 +1,29 @@
-package com.example.dream_shops;
+package com.example.dream_shops.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL )
+    private List<Order> orders;
+
+    
 }
