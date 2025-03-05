aada3e55eb79c7204d3277b89847daeced7e2783
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-03-05 08:10:37 -0500

    Create AdminService.java
    created service to check admin with related functions to check and delete admin

---
+++ dream-shops/src/main/java/com/example/dream_shops/service/AdminService.java
@@ -0,0 +1,43 @@
+package com.example.dream_shops.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dream_shops.repos.AdminRepo;
+import com.example.dream_shops.entity.Admin;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepo adminRepo;
+
+    public List<Admin> getAllAdmin(){
+        return adminRepo.findAll();
+    }
+
+    public Admin getAdminById(Long id){
+        return adminRepo.findById(id).orElseThrow(() ->new RuntimeException("Admin with id " + id + " not found"));
+    }
+
+    public void updateAdmin(Admin admin){
+        adminRepo.findById(admin.getId()).orElseThrow(() -> new RuntimeException("Amin with id" + admin.getId() + " not found"));
+        adminRepo.save(admin);
+    }
+
+    public void deleteAdmin(Long id){
+        adminRepo.findById(id).orElseThrow(() -> new RuntimeException("Admin with id "+ id + " not found"));
+        adminRepo.deleteById(id);
+    }
+
+    public boolean verifyCredentials(String email, String password){
+        Admin admin = adminRepo.findByEmail(email);
+        if (admin.getPassword() == password){
+            return true;
+        }
+        return false;
+    }
+
+    }        
