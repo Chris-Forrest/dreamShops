30ed0111c2bda071466017b143e1b14b671b1540
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-03-06 12:21:58 -0500

    Update AdminController.java
    completed initial code for admin controller

--- dream-shops/src/main/java/com/example/dream_shops/controllers/AdminController.java
+++ dream-shops/src/main/java/com/example/dream_shops/controllers/AdminController.java
@@ -3,8 +3,11 @@ package com.example.dream_shops.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.dream_shops.entity.Admin;
 import com.example.dream_shops.entity.Product;
@@ -40,6 +43,47 @@ public class AdminController {
         return "LoginPage";
     }
 
-    
+    @GetMapping("/admin/home")
+    public String adminHomePage(Model model){
+        model.addAttribute("adminList", adminService.getAllAdmin());
+        model.addAttribute("userList", userService.getAllUser());
+        model.addAttribute("orderList", orderService.getAllOrder());
+        model.addAttribute("productList", productService.getAllProduct());
+
+        return "AdminHomePage";
+    }
+
+    @GetMapping("/add/admin")
+    public String createAdmin(){
+        return "AddAdmin";
+    }
+
+    @PostMapping("/add/admin")
+    public String createAdmin(Admin admin){
+        adminService.createUser(admin);
+
+        return "/admin/home";
+    }
+
+    @GetMapping("/update/admin/{id}")
+    public String updateAdmin(@PathVariable Long id, Model model){
+        model.addAttribute("admin", adminService.getAdminById(id));
+
+        return "UpdateAdmin";
+    }
+
+    @PostMapping("/update/admin")
+    public String updateAdmin(Admin admin){
+        adminService.updateAdmin(admin);
+
+        return "/admin/home";
+    }
+
+    @DeleteMapping("/delete/admin/{id}")
+    public String deleteAdmin(@PathVariable Long id){
+        adminService.deleteAdmin(id);
+
+        return "/admin/home";
+    }
 
 }
