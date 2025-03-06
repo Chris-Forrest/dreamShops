95cb51f006b317874d9507a579d11ea544a22b89
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-03-06 12:35:47 -0500

    Create UserController.java
    initial code and functions for user controller

---
+++ dream-shops/src/main/java/com/example/dream_shops/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.example.dream_shops.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.dream_shops.entity.User;
+import com.example.dream_shops.service.UserService;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
+
+@Controller
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/add/user")
+    public  String addUser(){
+        return "AddUser";
+    }
+
+    @PostMapping("/add/user")
+    public String addUser(User user){
+        userService.createUser(user);
+
+        return "Login";
+    }
+
+    @GetMapping("/update/user/{id}")
+    public String updateUser(@PathVariable Long id, Model model){
+        model.addAttribute("admin", userService.getUserById(id));
+
+        return "UpdateUser";
+    }
+
+    @PostMapping("/update/user")
+    public String updateUser(User user){
+        userService.createUser(user);
+
+        return "/admin/home";
+    }
+
+    @DeleteMapping("/delete/user/{id}")
+    public String deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+
+        return "redirect:/admin/home";
+    }
+
+    
+}
