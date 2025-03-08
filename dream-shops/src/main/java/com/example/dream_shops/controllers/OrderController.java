345d9d5a4df6ce357c99372936528c9a714eec96
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-03-08 11:52:31 -0500

    Update OrderController.java

--- dream-shops/src/main/java/com/example/dream_shops/controllers/OrderController.java
+++ dream-shops/src/main/java/com/example/dream_shops/controllers/OrderController.java
@@ -1,5 +1,25 @@
 package com.example.dream_shops.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.dream_shops.entity.Order;
+import com.example.dream_shops.service.OrderService;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/place/order")
+    public String placeOrder(Order order, Model model){
+        double totalAmount = order.getPrice() * order.getQuantity();
+        
+        return "/admin/page";
+    }
 
 }
